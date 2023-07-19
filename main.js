68d3c2885225f2996b5a4347c9e617cff24dbcf5
digitaldeja0 <90660363+digitaldeja0@users.noreply.github.com>
2023-07-19 09:39:25 -0400

    Add screen size check and update styles.

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import * as BABYLON from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
 import { Inspector } from "@babylonjs/inspector";
 
-const totalSpan = document.querySelector("#totalSpan")
-document.querySelector("#popupBtn").addEventListener("click", ()=>{
-  document.querySelector("#popup").style.display="none"
-  document.querySelector("#overlay").style.display="flex"
-})
+const totalSpan = document.querySelector("#totalSpan");
+document.querySelector("#popupBtn").addEventListener("click", () => {
+  document.querySelector("#popup").style.display = "none";
+  document.querySelector("#overlay").style.display = "flex";
+});
 const canvas = document.getElementById("canvas");
 const engine = new BABYLON.Engine(canvas, true);
 
@@ -55,7 +55,6 @@ async function createScene() {
     scene
   );
 
-
   const resultBottles = await BABYLON.SceneLoader.ImportMeshAsync(
     "",
     "./models/",
@@ -70,35 +69,37 @@ async function createScene() {
   const bottle3 = scene.getNodeByName("bottle.003");
   const bottle4 = scene.getNodeByName("bottle.004");
 
-  let totalBottles = 0
+  let totalBottles = 0;
 
   scene.onPointerObservable.add((pointerInfo) => {
-    if(pointerInfo.type === BABYLON.PointerEventTypes.POINTERDOWN){
-      if(pointerInfo.pickInfo.pickedMesh!== null &&pointerInfo.pickInfo.pickedMesh.name){
-      switch(pointerInfo.pickInfo.pickedMesh.name){
-        case "bottle_primitive0":
-        case "bottle_primitive1":
-        case "bottle_primitive2":
-        case "bottle.001_primitive0":
-        case "bottle.001_primitive1":
-        case "bottle.001_primitive2":
-        case "bottle.002_primitive0":
-        case "bottle.002_primitive1":
-        case "bottle.002_primitive2":
-        case "bottle.003_primitive0":
-        case "bottle.003_primitive1":
-        case "bottle.003_primitive2":
-        case "bottle.004_primitive0":
-        case "bottle.004_primitive1":
-        case "bottle.004_primitive2":
-          console.log("Bottle Selected")
-          totalBottles++
-          totalSpan.textContent = totalBottles.toString()
-          pointerInfo.pickInfo.pickedMesh.parent.dispose()
-        break
+    if (pointerInfo.type === BABYLON.PointerEventTypes.POINTERDOWN) {
+      if (
+        pointerInfo.pickInfo.pickedMesh !== null &&
+        pointerInfo.pickInfo.pickedMesh.name
+      ) {
+        switch (pointerInfo.pickInfo.pickedMesh.name) {
+          case "bottle_primitive0":
+          case "bottle_primitive1":
+          case "bottle_primitive2":
+          case "bottle.001_primitive0":
+          case "bottle.001_primitive1":
+          case "bottle.001_primitive2":
+          case "bottle.002_primitive0":
+          case "bottle.002_primitive1":
+          case "bottle.002_primitive2":
+          case "bottle.003_primitive0":
+          case "bottle.003_primitive1":
+          case "bottle.003_primitive2":
+          case "bottle.004_primitive0":
+          case "bottle.004_primitive1":
+          case "bottle.004_primitive2":
+            totalBottles++;
+            totalSpan.textContent = totalBottles.toString();
+            pointerInfo.pickInfo.pickedMesh.parent.dispose();
+            break;
+        }
       }
     }
-  }
   });
 
   return scene;
@@ -106,49 +107,95 @@ async function createScene() {
 
 const scene = await createScene();
 
-const gameOverStat = false
+const gameOverStat = false;
 engine.runRenderLoop(() => {
   scene.render();
-  if(totalSpan.textContent ==="5"){
-    gameOver()
+  if (totalSpan.textContent === "5") {
+    gameOver();
   }
 });
 window.addEventListener("resize", () => {
   engine.resize();
 });
 
-const gameOver =()=>{
+const gameOver = () => {
   var newPopupDiv = document.createElement("div");
   newPopupDiv.setAttribute("id", "newpopup");
   newPopupDiv.setAttribute("class", "popupDiv");
 
   // Create the paragraph element
   var newParagraph = document.createElement("p");
-  var paragraphText = document.createTextNode("You take the potion and the world goes black. You wake up in your bed. It was all a dream. On your lap a slice fo 3 day old pizza. You'll never order from Dominos again.");
+  var paragraphText = document.createTextNode(
+    "You take the potion and the world goes black."
+  );
   newParagraph.appendChild(paragraphText);
+
+  var newParagraph2 = document.createElement("p");
+  var paragraphText2 = document.createTextNode(
+    "You wake up in your bed, it was all a dream."
+  );
+  newParagraph2.appendChild(paragraphText2);
+
+  var newParagraph3 = document.createElement("p");
+  var paragraphText3 = document.createTextNode(
+    "On your lap a slice of 3 day old pizza. "
+  );
+  newParagraph3.appendChild(paragraphText3);
+
+  var newParagraph4 = document.createElement("p");
+  var paragraphText4 = document.createTextNode(
+    " You'll never order from Dominos again."
+  );
+  newParagraph4.appendChild(paragraphText4);
+
+  // Add classes
+  newParagraph.setAttribute("class", "newPopP");
+  newParagraph2.setAttribute("class", "newPopP");
+  newParagraph3.setAttribute("class", "newPopP");
+  newParagraph4.setAttribute("class", "newPopP");
 
   // Create the button element
   var newButton = document.createElement("button");
-  var buttonText = document.createTextNode("Continue Exploring");
+  var buttonText = document.createTextNode("You Win!");
   newButton.appendChild(buttonText);
   newButton.setAttribute("class", "btn");
-  console.log(newButton)
 
   // Add an event listener to the button
-  newButton.addEventListener("click", (e)=> {
-    e.preventDefault()
-      newPopupDiv.remove()
+  newButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    newPopupDiv.remove();
   });
 
   // Append paragraph and button elements to the div
   newPopupDiv.appendChild(newParagraph);
+  newPopupDiv.appendChild(newParagraph2);
+  newPopupDiv.appendChild(newParagraph3);
+  newPopupDiv.appendChild(newParagraph4);
   newPopupDiv.appendChild(newButton);
 
   // Add the new popup div to the body of the document
   document.body.appendChild(newPopupDiv);
 
-  totalSpan.textContent =null
+  totalSpan.textContent = null;
 
-  document.querySelector("#totalP").textContent ="Game Over"
+  document.querySelector("#totalP").textContent = "Game Over";
+};
 
-}
+window.addEventListener('load', ()=> {
+  const fullscreenDiv = document.querySelector('.fullscreen');
+
+  function checkScreenWidth() {
+    if (window.innerWidth < 800) {
+      fullscreenDiv.classList.add('show-message');
+      console.log("small")
+    } else {
+      fullscreenDiv.classList.remove('show-message');
+    }
+  }
+
+  // Check on page load
+  checkScreenWidth();
+
+  // Check on window resize
+  window.addEventListener('resize', checkScreenWidth);
+});
